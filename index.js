48ae92d15f8e3dfda9829e1ab5b6c76dacc03b1b
dburns98 <dylanburns698@gmail.com>
2022-09-06 22:17:27 -0400

    made questions that the user will be asked

--- index.js
+++ index.js
@@ -1,13 +1,60 @@
-// TODO: Include packages needed for this application
+// packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// an array of questions for user input
+const questions = [
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'title',
+                message: 'What is the title of your project?',
+            },
+            {
+                type: 'input',
+                name: 'description',
+                message: 'Provide a short description explaining the what, why, and how of your project.',
+            },
+            {
+                type: 'input',
+                name: 'installation',
+                message: 'What are the steps required to install your project?',
+            },
+            {
+                type: 'input',
+                name: 'usage',
+                message: 'Provide instructions and examples for use.',
+            },
+            {
+                type: 'input',
+                name: 'credits',
+                message: 'List your colloaborators, If any, with links to their github profiles.',
+            },
+            {
+                type: 'list',
+                name: 'license',
+                message: 'Which License will you use for your project?',
+                choices: ['agp1', 'apache', 'mit', 'no license']
+            },
+            {
+                type: 'input',
+                name: 'test',
+                message: 'Provide instructions on how to test the application',
+            },
+        
+        ])
+];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// function to write README file
+function writeToFile(fileName, data) {
 
-// TODO: Create a function to initialize app
-function init() {}
+}
+
+// function to initialize app
+function init() {
+
+}
 
 // Function call to initialize app
 init();
